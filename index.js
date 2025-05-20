4de043e38994bd01b7a9cd66f8780ed3955f0b57
martynamazur <127408392+martynamazur@users.noreply.github.com>
2025-05-20 09:35:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post('/payments/googlepay', async (req, res) => {
       body: JSON.stringify({
         pay: {
           groupId: parseInt(process.env.TPAY_GROUP_ID, 10), 
-          amount: 4.00, 
+          amount: '4.00', 
           description: description || 'Zakup biletu',
           googlePayPaymentData: encodedToken
         },
